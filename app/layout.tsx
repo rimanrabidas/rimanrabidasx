3d248cb6fe13528ebf428562e0390e6d26a35a68
Riman Rabidas <rimanblp@gmail.com>
2026-09-06 20:16:24 -0300

    refresh p..

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 
 export const metadata: Metadata = {
-  title: "Your Name | Full Stack Developer",
+  title: "Riman Rabidas | Full Stack Developer",
   description: "Portfolio of a Full Stack Developer",
 
   openGraph: {
