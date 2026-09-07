1d3ba76c8b4b8f8faa9c80a4735204f95e0bce92
Riman Rabidas <rimanblp@gmail.com>
2026-09-06 22:18:47 -0300

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export const metadata: Metadata = {
     siteName: "Riman Rabidas",
     images: [
       {
-        url: "/mylogo.png",
+        url: "/mymetadata.png",
         width: 1200,
         height: 630,
         alt: "Riman Rabidas Portfolio",
