c8f945585c825d3f8d68a0fa8eecab1c2cc1724e
Riman Rabidas <rimanblp@gmail.com>
2026-09-06 22:08:57 -0300

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 const siteUrl =
-  process.env.NEXT_PUBLIC_SITE_URL || "https://rimanrabidas.vercel.app";
+  process.env.NEXT_PUBLIC_SITE_URL || "https://rimanrabidas.netlify.app";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteUrl),
